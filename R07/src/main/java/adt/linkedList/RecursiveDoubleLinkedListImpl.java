c5345703185bca3359c9aa0b5d1b50ad544aa398
alexmof <alex.fidelis@ccc.ufcg.edu.br>
2019-09-26 15:52:10 -0300

    DoubleLL start

--- R07/src/main/java/adt/linkedList/RecursiveDoubleLinkedListImpl.java
+++ R07/src/main/java/adt/linkedList/RecursiveDoubleLinkedListImpl.java
@@ -11,8 +11,12 @@ public class RecursiveDoubleLinkedListImpl<T> extends
 
 	@Override
 	public void insertFirst(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		T aux = this.getData();
+		this.setData(element);
+		RecursiveDoubleLinkedListImpl<T> second = new RecursiveDoubleLinkedListImpl<T>();
+		second.setData(aux);
+		
+		RecursiveDoubleLinkedListImpl<T> third = this.ge
 	}
 
 	@Override
